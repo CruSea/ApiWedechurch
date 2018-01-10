1d207b5ea143569345f312e54c24a6eac258f4e1
frezertade <frezertade@gmail.com>
2018-01-10 17:09:11 +0300

    check

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,7 +10,8 @@ class APIController extends Controller
 	
 
     public function register(Request $request)
-    {        
+    {     
+        print 'heey';   
     	 $input = $request->all();
     	// $input['password'] = Hash::make($input['password']);
     	// User::create($input);
