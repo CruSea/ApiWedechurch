295ee68e50faa7ae86b04041de6a2cd1b63e3bae
frezertade <frezertade@gmail.com>
2018-01-20 16:21:08 +0300

    new login

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -55,18 +55,18 @@ class APIController extends Controller
     //     return response()->json(['status'=>true,'message'=>'User created successfully','data'=>$user]);
     // }
     
-    // public function login(Request $request){
-    //     $credentials = $request->only('email', 'password');
-    //     $token = null;
-    //     try {
-    //        if (!$token = JWTAuth::attempt($credentials)) {
-    //         return response()->json(['invalid_email_or_password'], 422);
-    //        }
-    //     } catch (JWTAuthException $e) {
-    //         return response()->json(['failed_to_create_token'], 500);
-    //     }
-    //     return response()->json(compact('token'));
-    // }
+    public function login(Request $request){
+        $credentials = $request->only('email', 'password');
+        $token = null;
+        try {
+           if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['invalid_email_or_password'], 422);
+           }
+        } catch (JWTAuthException $e) {
+            return response()->json(['failed_to_create_token'], 500);
+        }
+        return response()->json(compact('token'));
+    }
     // public function getAuthUser(Request $request){
     //     $user = JWTAuth::toUser($request->token);
     //     return response()->json(['result' => $user]);
